c15ff8e284727d6a788c15d856cbb050fb2bfd53
Melanie Verron <me.verron@laposte.net>
2020-01-15 15:55:00 +0100

    Display pizza list and selected pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,14 @@ interface Pizza {
   price: number;
 }
 
+const PIZZAS : Pizza[] = [
+  { id: 1, name: 'Reine', price: 12 },
+  { id: 2, name: '4 fromages', price: 13 },
+  { id: 3, name: 'Orientale', price: 11 },
+  { id: 4, name: 'Cannibale', price: 9 }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,9 +21,11 @@ interface Pizza {
 })
 export class AppComponent {
   title = 'pizzaparty';
-  pizza: Pizza = {
-    id: 1,
-    name : 'Reine',
-    price: 12
-  };
+  selectedPizza: Pizza; 
+  pizzas:Pizza[] = PIZZAS; 
+
+  onSelect(pizza: Pizza) : void {
+    // on récupère la pizza cliquée
+    console.log(pizza); 
+  }
 }
