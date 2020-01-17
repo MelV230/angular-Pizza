e7ae30052bcf55af62da53a44169fb52d413209a
Melanie Verron <me.verron@laposte.net>
2020-01-17 12:59:27 +0100

    Configure Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from "@angular/router"; 
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { AuthorComponent } from './author/author.component';
 import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
 import { MenuComponent } from './menu/menu.component';
-import { TaxPipe } from './pipe/tax.pipe';
+import { TestPipe } from './test.pipe';
+import { TaxPipe } from './tax.pipe';
 import { HomeComponent } from './home/home.component';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { PizzaSingleComponent } from './pizza-single/pizza-single.component';
 
 @NgModule({
   declarations: [
@@ -18,16 +21,20 @@ import { HomeComponent } from './home/home.component';
     AuthorComponent,
     IngredientListComponent,
     MenuComponent,
+    TestPipe,
     TaxPipe,
     HomeComponent,
-    
+    PizzaListComponent,
+    PizzaSingleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      // Un objet repésente une route 
-      { path: '', component: HomeComponent }
+      // Un objet représente une route
+      { path: '', component: HomeComponent },
+      { path: 'pizzas', component: PizzaListComponent },
+      { path: 'pizzas/:id', component: PizzaSingleComponent }
     ])
   ],
   providers: [],
